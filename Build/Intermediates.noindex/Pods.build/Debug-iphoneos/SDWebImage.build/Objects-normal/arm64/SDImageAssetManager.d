1dae572210c265341777cbeb3a0c0b21d75e83ab
Adam Rowe <adaminsley@gmail.com>
2020-06-17 17:48:40 -0400

    detail view

--- Build/Intermediates.noindex/Pods.build/Debug-iphoneos/SDWebImage.build/Objects-normal/arm64/SDImageAssetManager.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Private/SDImageAssetManager.m \
-  /Users/adam/Projects/itunes_rss/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/SDWebImage.build/module.modulemap \
-  /Users/adam/Projects/itunes_rss/Pods/Target\ Support\ Files/SDWebImage/SDWebImage-prefix.pch \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Private/SDImageAssetManager.h \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Core/SDWebImageCompat.h \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Private/SDInternalMacros.h \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Private/SDmetamacros.h
--- Build/Intermediates.noindex/Pods.build/Debug-iphoneos/SDWebImage.build/Objects-normal/arm64/SDWebImageError.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Core/SDWebImageError.m \
-  /Users/adam/Projects/itunes_rss/Build/Intermediates.noindex/Pods.build/Debug-iphoneos/SDWebImage.build/module.modulemap \
-  /Users/adam/Projects/itunes_rss/Pods/Target\ Support\ Files/SDWebImage/SDWebImage-prefix.pch \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Core/SDWebImageError.h \
-  /Users/adam/Projects/itunes_rss/Pods/SDWebImage/SDWebImage/Core/SDWebImageCompat.h
