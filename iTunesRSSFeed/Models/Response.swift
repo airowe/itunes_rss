6a5e17334bd93bac76835662521bf817227570a1
Adam Rowe <adaminsley@gmail.com>
2020-06-17 17:52:17 -0400

    remove untracked files

---
+++ iTunesRSSFeed/Models/Response.swift
@@ -0,0 +1,13 @@
+//
+//  Response.swift
+//  iTunesRSSFeed
+//
+//  Created by Adam Rowe on 6/17/20.
+//  Copyright © 2020 Adam Rowe. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Codable {
+    let feed: Feed
+}
---
+++ iTunesRSSFeed/Networking/APIClient.swift
@@ -0,0 +1,29 @@
+//
+//  APIClient.swift
+//  iTunesRSSFeed
+//
+//  Created by Adam Rowe on 6/16/20.
+//  Copyright © 2020 Adam Rowe. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+struct APIClient {
+
+    struct Response<T> {
+        let value: T
+        let response: URLResponse
+    }
+
+    func run<T: Decodable>(_ request: URLRequest) -> AnyPublisher<Response<T>, Error> {
+        return URLSession.shared
+            .dataTaskPublisher(for: request)
+            .tryMap { result -> Response<T> in
+                let value = try JSONDecoder().decode(T.self, from: result.data)
+                return Response(value: value, response: result.response)
+        }
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+    }
+}
